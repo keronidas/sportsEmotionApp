781c7698f6da40bdb992c349b6937dca75244069
keronidas <diegojuliandev@gmail.com>
2025-01-24 20:00:53 +0100

    implementando signals

--- src/app/pages/task-view/task-view.component.ts
+++ src/app/pages/task-view/task-view.component.ts
@@ -1,16 +1,35 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TasksDto } from '../../interfaces/task.interface';
+import { TasksService } from '../../services/tasks.service';
 
 @Component({
   selector: 'app-task-view',
-  imports: [],
   templateUrl: './task-view.component.html',
-  styleUrl: './task-view.component.scss'
+  styleUrls: ['./task-view.component.scss']
 })
-export class TaskViewComponent implements OnInit {
+export class TaskViewComponent implements OnInit {  // Cambié AfterViewInit a OnInit
   id!: string;
-  private route = inject(ActivatedRoute)
-  ngOnInit() {
+  tasks: WritableSignal<TasksDto[] | null> = signal(null); // Inicializamos con null
+
+  private route = inject(ActivatedRoute);
+  private taskService = inject(TasksService);
+
+  ngOnInit(): void {  // Cambié ngAfterViewInit a ngOnInit
+    // Obtener el parámetro 'id' desde la URL
     this.id = this.route.snapshot.paramMap.get('id') ?? '';
+
+    // Si tenemos un ID, hacemos la solicitud
+    if (this.id) {
+      this.taskService.getTasksById(Number(this.id)).subscribe({
+        next: (task) => {
+          console.log('Tarea recibida:', task); // Verifica la tarea que se recibe
+          this.tasks.set([task]); // Almacenamos la tarea obtenida
+        },
+        error: (err) => {
+          console.error('Error al obtener la tarea:', err);
+        }
+      });
+    }
   }
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { TasksDto } from '../interfaces/task.interface';
 
@@ -8,13 +8,23 @@ import { TasksDto } from '../interfaces/task.interface';
   providedIn: 'root'
 })
 export class TasksService {
-
-
   private http = inject(HttpClient)
   private apiUrl = environment.apiUrl + '/todos';
 
 
   getTasks(): Observable<TasksDto> {
     return this.http.get<TasksDto>(this.apiUrl)
+  }
+
+  getTasksById(userId: number): Observable<TasksDto> {
+    const url = `${this.apiUrl}?userId=${userId}`;
+
+
+    return this.http.get<TasksDto>(url).pipe(
+      tap((response) => {
+        // Aquí mostramos la respuesta del servidor
+        console.log('Respuesta recibida en el servicio:', response);
+      })
+    );
   }
 }
