43656e9243ed7dc2933718820c208ed32132f7b9
Diego Julian Rodriguez <141148033+keronidas@users.noreply.github.com>
2025-01-27 00:37:48 +0100

    solucionando errores en testing side-bar

---
+++ src/app/shared/components/side-bar/side-bar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SideBarComponent } from './side-bar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('SideBarComponent', () => {
+  let component: SideBarComponent;
+  let fixture: ComponentFixture<SideBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SideBarComponent, RouterTestingModule],
+      providers: [
+
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SideBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
